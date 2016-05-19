7284435e9dfa64fe7ec302b49e4da5f21b3ac857
Jacob Colvin <jacobcolvin1@gmail.com>
2016-05-19 15:43:29 -0400

    Update new_relic_agent.rb

--- lib/java_buildpack/framework/new_relic_agent.rb
+++ lib/java_buildpack/framework/new_relic_agent.rb
@@ -64,7 +64,7 @@ module JavaBuildpack
       def apply_configuration(credentials, configuration)
         configuration['log_file_name'] = 'STDOUT'
         configuration[LICENSE_KEY_USER] = credentials[LICENSE_KEY]
-        configuration['app_name'] = @application.details['application_name']
+        configuration['app_name'] = @application.details['application_name'] + '-' + @application.details['space_name']
       end
 
       def apply_user_configuration(credentials, configuration)
